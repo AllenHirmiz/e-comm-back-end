ae2017f45ca8e98c6ec609854393747b4b95953b
AllenHirmiz <allenaliraqi@gmail.com>
2023-08-17 01:52:45 +1000

    Create routs and api endpoints codes

--- main/routes/index.js
+++ main/routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route!</h1><br>Try the following routs:<br>/api/tags<br>/api/products<br>/api/categories")
 });
 
 module.exports = router;
